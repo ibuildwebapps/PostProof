1d360b663a76be6de46dddcab9ccde5faa170b02
paul <paul>
2021-10-18 10:29:37 +0100

    Laravel framework configured

---
+++ app/Http/Controllers/APIListenerController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class APIListenerController extends Controller
+{
+    //This is a catch-all for /listener endpoint
+    public function index()
+    {
+
+    }
+}
