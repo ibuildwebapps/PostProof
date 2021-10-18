94e955f3b97881943b0b8e92f6f41273e560d548
paul <paul>
2021-10-18 16:25:48 +0100

    Juggling arrays

--- app/Http/Controllers/APIListenerController.php
+++ app/Http/Controllers/APIListenerController.php
@@ -13,72 +13,89 @@ class APIListenerController extends Controller
     //This is a catch-all for /listener endpoint
     public function index(Request $request, $tag = null)
     {
-        $method = $request->method() ;
-        $expectedContentTypes = $request->getAcceptableContentTypes() ;
-        $clientIPs = $request->getClientIps() ;
-        $defaultLocale = $request->getDefaultLocale() ;
-        $scheme = $request->getScheme() ;
-        $postData = $request->post() ;
-        $getData = $request->query() ;
-        $requestHeaders = $request->header() ;
+        $method = $request->method();
+        $expectedContentTypes = $request->getAcceptableContentTypes();
+        $clientIPs = $request->getClientIps();
+        $defaultLocale = $request->getDefaultLocale();
+        $scheme = $request->getScheme();
+        $postData = $request->post();
+        $getData = $request->query();
+        $requestHeaders = $request->header();
 
-        Log::info('Tag: ' . $tag) ;
-        Log::info('Scheme: ' . $scheme) ;
-        Log::info('Method: ' . $method) ;
-        Log::info('User Agent: ' . $request->userAgent()) ;
-        Log::info('Expected Content Types: ' . json_encode($expectedContentTypes)) ;
-        Log::info('Client IPs: ' . json_encode($clientIPs)) ;
-        Log::info('Default Locale: ' . $defaultLocale) ;
-        Log::info('POST Data: ' . json_encode($postData)) ;
-        Log::info('GET Data: ' . json_encode($getData)) ;
-        Log::info('Headers: ') ;
-        foreach($requestHeaders AS $k => $v)
-            if(count($v) > 1)
-                Log::info($k . ' = ' . json_encode($v)) ;
-            else
-                Log::info($k . ' = ' . $v[0]) ;
+//        Log::info('Tag: ' . $tag);
+//        Log::info('Scheme: ' . $scheme);
+//        Log::info('Method: ' . $method);
+//        Log::info('User Agent: ' . $request->userAgent());
+//        Log::info('Expected Content Types: ' . json_encode($expectedContentTypes));
+//        Log::info('Client IPs: ' . json_encode($clientIPs));
+//        Log::info('Default Locale: ' . $defaultLocale);
+//        Log::info('POST Data: ' . json_encode($postData));
+//        Log::info('GET Data: ' . json_encode($getData));
+//        Log::info('Headers: ');
+//        foreach ($requestHeaders as $k => $v)
+//            if (count($v) > 1)
+//                Log::info($k . ' = ' . json_encode($v));
+//            else
+//                Log::info($k . ' = ' . $v[0]);
+//
+//        Log::info('creating hit');
 
-         $hit = Hit::create(['tag' => $tag ?? null,
-                        'scheme' => $scheme,
-                                        'method' => $method,
-                                        'expected_content_types' => json_encode($expectedContentTypes),
-                                        'client_ips' => json_encode($clientIPs),
-                                        'default_locale' => $defaultLocale,
-                                        'user_agent' => $request->userAgent(),
-                                        'post_data' => json_encode($postData),
-                                        'get_data' => json_encode($getData),
-                                        'headers' => json_encode($requestHeaders)]) ;
-        if($hit)
+        $hit = Hit::create(['tag'                    => $tag ?? null,
+                            'scheme'                 => $scheme,
+                            'method'                 => $method,
+                            'expected_content_types' => json_encode($expectedContentTypes),
+                            'client_ips'             => json_encode($clientIPs),
+                            'default_locale'         => $defaultLocale,
+                            'user_agent'             => $request->userAgent(),
+                            'post_data'              => json_encode($postData),
+                            'get_data'               => json_encode($getData),
+                            'headers'                => json_encode($requestHeaders)]);
+        //Log::info('hit created');
+
+        if ($hit)
         {
             //Write POST
-            foreach($postData AS $k => $v)
+            foreach ($postData as $k => $v)
             {
-                Metadata::create(['fk_hit_id' => $hit->id,
-                                     'metadata_key' => $k,
-                                     'metadata_value' => $v,
-                                     'metadata_type' => 'POST']) ;
+                Metadata::create(['fk_hit_id'      => $hit->id,
+                                  'metadata_key'   => $k,
+                                  'metadata_value' => $v,
+                                  'metadata_type'  => 'POST']);
             }
             //Write GET
-            foreach($getData AS $k => $v)
+            foreach ($getData as $k => $v)
             {
-                Metadata::create(['fk_hit_id' => $hit->id,
-                                  'metadata_key' => $k,
+                Metadata::create(['fk_hit_id'      => $hit->id,
+                                  'metadata_key'   => $k,
                                   'metadata_value' => $v,
-                                  'metadata_type' => 'GET']) ;
+                                  'metadata_type'  => 'GET']);
             }
             //Write Headers
-            foreach($requestHeaders AS $k => $v)
+            foreach ($requestHeaders as $k => $v)
             {
-                Metadata::create(['fk_hit_id' => $hit->id,
-                                  'metadata_key' => $k,
-                                  'metadata_value' => $v,
-                                  'metadata_type' => 'HEADER']) ;
+                if (is_array($v))
+                {
+                    foreach ($v as $v1)
+                    {
+                        Metadata::create(['fk_hit_id'      => $hit->id,
+                                          'metadata_key'   => $k,
+                                          'metadata_value' => $v1,
+                                          'metadata_type'  => 'HEADER']);
+                    }
+                }
+                else
+                {
+                    Metadata::create(['fk_hit_id'      => $hit->id,
+                                      'metadata_key'   => $k,
+                                      'metadata_value' => $v,
+                                      'metadata_type'  => 'HEADER']);
+                }
             }
 
             //If we are doing a passthrough, make the request here (refactor to helper function)
             return Response::HTTP_OK;
         }
 
-        return Response::HTTP_INTERNAL_SERVER_ERROR ;
+        return Response::HTTP_INTERNAL_SERVER_ERROR;
     }
 }
